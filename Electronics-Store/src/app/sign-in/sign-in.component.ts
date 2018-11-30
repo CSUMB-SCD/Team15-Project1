6b119c1308b85c754a4d1c4bb79b96e2d2146e76
Manjit Gurcha <mgurcha@csumb.edu>
2018-11-30 15:09:23 -0800

    Adding the changes to sign in and products page

--- Electronics-Store/src/app/sign-in/sign-in.component.ts
+++ Electronics-Store/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  users$: Object;
+  login_message = 'yes';
 
-  constructor() { }
+  constructor(private data: DataService, private router: Router) {}
 
-  ngOnInit() {
+  onSubmit(username, password) {
+    let success = false;
+
+      if (this.users$[0].username === username
+        && this.users$[0].password === password) {
+        success = true;
+      }
+      if (this.users$[1].username === username
+        && this.users$[1].password === password) {
+        success = true;
+      }
+      if (this.users$[2].username === username
+        && this.users$[2].password === password) {
+        success = true;
+      }
+//////
+
+    if (success) {
+      this.login_message = 'yes';
+      this.newMessage(this.login_message);
+    }
+    if(success == false){
+      this.login_message = 'no';
+      this.newMessage(this.login_message);
+    }
   }
 
+  ngOnInit() {
+    this.data.getUsers().subscribe(data => this.users$ = data);
+    this.data.currentStatus.subscribe(message => this.login_message = message);
+
+  }
+
+  newMessage(changeStatus) {
+    this.data.changeMessage(changeStatus)
+  }
+  signOut() {
+    this.login_message = 'no';
+    this.newMessage(this.login_message);
+  }
 }
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
