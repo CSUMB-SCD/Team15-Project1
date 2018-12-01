e9c54ebc69561a6f01f06570ba616267c77efcf0
Raeleen Watson <raeleenwatson@Raeleens-MacBook-Pro.local>
2018-11-30 21:44:10 -0800

    Refactored code and added DataService for home comp.

--- Electronics-Store/src/app/home/home.component.ts
+++ Electronics-Store/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { NgModule }      from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -10,53 +11,26 @@ import { NgModule }      from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  private infos = [];
-
-  name:string = '';
-  productInfoUrl:string = 'https://spring-boot-testing-438.herokuapp.com/allProductInfo';
-  price: number;
-  productDesc:string = '';
-  found:boolean;
+  items$: Object;
   
-  constructor(private httpClient:HttpClient) { }
+   name:string = '';
+   found:boolean;
+
+  constructor(private dataService: DataService) { }
+
   onNameKeyUp(event:any) {
      this.name = event.target.value;
      this.found = false;
   }
 
-   getProductInfoURL(name: string) : string {
-    return this.productInfoUrl + '?productName=' + name;
+  getProductInfo(){
+    this.dataService.getProductInfoByName(this.name).subscribe(data => this.items$ = data);
+     console.log(this.items$)
+     this.found = true;
   }
-
-  getProductDescription(){
-   
-    this.httpClient.get(this.getProductInfoURL(this.name))
-    .subscribe(
-      (data:any[]) => {
-        
-         if(data.length){
-           //console.log(data)
-           this.infos = data;
-           let a =0;
-           for(a;a<this.infos.length;a++){
-             if(this.infos[a].productName==this.name)
-             {
-              this.price = this.infos[a].price;
-
-             }
-           }
-           console.log(this.infos); 
-            this.found = true;
-         }
-      }
-    )
-  }
-
-  //heroku and add mLab plugin
-  //basic empty app, connect app to mLab (need URI)
-  
 
   ngOnInit() {
+    
   }
 
 }
