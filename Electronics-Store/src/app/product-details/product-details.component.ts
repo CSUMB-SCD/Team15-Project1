bc3b0646f028626c588698b5effc3a35a9d9a558
Raeleen Watson <raeleenwatson@Raeleens-MacBook-Pro.local>
2018-12-01 01:39:34 -0800

    Saving working changes

--- Electronics-Store/src/app/product-details/product-details.component.ts
+++ Electronics-Store/src/app/product-details/product-details.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../data.service';
-import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-product-details',
@@ -13,16 +12,24 @@ export class ProductDetailsComponent implements OnInit {
   item$
 
   constructor(private route: ActivatedRoute, private data: DataService) {
-    //this.route.queryParamMap.pipe(map(params => console.log(params)));
+    
    }
 
+   getAllProductDetails(){
+
+    const param: string = this.route.snapshot.queryParamMap.get('productName');
+    console.log(param);
+
+    this.data.getProductInfoByName(param).subscribe(
+      data => this.item$ = data
+      );
+   }
+
+
   ngOnInit() {
-    //this.route.queryParamMap.pipe(map(params => console.log(params)));
-    this.route.params.subscribe( params => this.item$ = params.productName);
-    console.log(this.item$)
-  // this.data.getProductInfoByName(this.item$.productName).subscribe(
-  //   data => this.item$ = data
-  // )
+
+    this.getAllProductDetails();
+
   }
 
 }
--- Electronics-Store/src/app/products-page/products-page.component.ts
+++ Electronics-Store/src/app/products-page/products-page.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { range } from 'rxjs';
 import { DataService } from '../data.service';
-import { Router, ActivatedRoute } from '@angular/router';
-
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
@@ -11,8 +7,6 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ProductsPageComponent implements OnInit {
   
-  //@Input() name: string;
-
   items$: Object;
 
   private products = [];
@@ -25,12 +19,6 @@ export class ProductsPageComponent implements OnInit {
   found:boolean;
   
   constructor(private dataService: DataService) {}
-
-  // viewProductDetails(productID: number){
-  //   this._router.navigate(['product-details',this.items$], {
-  //     queryParams: { 'productName': this.name}
-  // });
-  // }
 
   postProductInfo() {
     this.dataService.getAllProductInfo().subscribe(
