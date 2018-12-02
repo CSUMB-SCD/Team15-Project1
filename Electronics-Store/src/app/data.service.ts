e6c736a428399fd86f9d0ae732c0a59377ac7e44
Manjit Gurcha <mgurcha@csumb.edu>
2018-12-01 22:00:03 -0800

    adding login capabilities

--- Electronics-Store/src/app/data.service.ts
+++ Electronics-Store/src/app/data.service.ts
@@ -16,14 +16,14 @@ export class DataService {
   }
 
   // functions to return JSON
-  getUsers() { return this.httpClient.get('../assets/users.json') }
+  getUsers() { return this.httpClient.get('https://team15spring-users.herokuapp.com/allUsers')}
 
   onNameKeyUp(event:any){
     console.log(event.target.value);
   }
 
   getProductInfoByName(productName) { return this.httpClient.get('https://spring-boot-testing-438.herokuapp.com/search?product-name='+ encodeURI(productName) )}
-  
+
   getAllProductInfo() {return this.httpClient.get('https://spring-boot-testing-438.herokuapp.com/allProductInfo') }
- 
+
 }
