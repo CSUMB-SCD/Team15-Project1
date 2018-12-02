3255355a438b7c186b41d1edd4af2cc14c9f200f
Manjit Gurcha <33430741+mgurcha@users.noreply.github.com>
2018-12-02 15:39:34 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,4 @@ app.get('/*', function(req, res) {
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 5000);
