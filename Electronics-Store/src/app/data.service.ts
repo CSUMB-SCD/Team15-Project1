c433268a8116796fe77ec7d56f40a5ad339f2a93
Irais1 <igopar-osorio@csumb.edu>
2018-12-04 18:40:54 -0800

    buttons

--- Electronics-Store/src/app/data.service.ts
+++ Electronics-Store/src/app/data.service.ts
@@ -6,24 +6,26 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   private loginStatus = new BehaviorSubject('no');
   currentStatus = this.loginStatus.asObservable();
 
   changeMessage(status: string) {
-    this.loginStatus.next(status)
+    this.loginStatus.next(status);
   }
 
   // functions to return JSON
-  getUsers() { return this.httpClient.get('../assets/users.json') }
+  getUsers() { return this.httpClient.get('https://team15spring-users.herokuapp.com/allUsers')}
 
-  onNameKeyUp(event:any){
+  onNameKeyUp(event: any) {
     console.log(event.target.value);
   }
 
-  getProductInfoByName(productName) { return this.httpClient.get('https://spring-boot-testing-438.herokuapp.com/search?product-name='+ encodeURI(productName) )}
-  
-  getAllProductInfo() {return this.httpClient.get('https://spring-boot-testing-438.herokuapp.com/allProductInfo') }
- 
+  getProductInfoByName(productName) {
+     return this.httpClient.get('https://spring-boot-testing-438.herokuapp.com/search?product-name=' + encodeURI(productName) );
+  }
+  getAllProductInfo() {
+    return this.httpClient.get('https://spring-boot-testing-438.herokuapp.com/allProductInfo');
+  }
 }
--- Electronics-Store/src/app/products-page/products-page.component.ts
+++ Electronics-Store/src/app/products-page/products-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { range } from 'rxjs';
 import { DataService } from '../data.service';
+import { $ } from 'protractor';
+
 
 @Component({
   selector: 'app-products-page',
@@ -29,32 +31,33 @@ export class ProductsPageComponent implements OnInit {
   constructor(private data: DataService) {}
 
   //FIX PROMPT BUTTONS TO YES/NO AND CHECKOUT/CONTINUE SHOPPING
-  addToCart(addedProducts){
+  addToCart(addedProducts) {
     this.addedProducts = addedProducts;
-    console.log(this.addedProducts)
-    
-    var confirm = window.confirm("Proceeding to add to cart...");
-    if(confirm==true){
-      var checkout = window.confirm("Would you like to checkout or continue shopping?")
-      if(checkout == true){
-        checkout=true;
-        console.log("Checking you out")
-      }
-      else{
-        console.log("Not checking you out")
-      }
-      //alert("Item was added to your cart")
-      //this.cart.push(addedProducts);
-      //console.log(this.cart)
-      
-    }
-    else{
-      alert("Item was not added to your cart");
-    }
+    console.log(this.addedProducts);
+
+    document.getElementById('id_confrmdiv').style.display = 'block'; //this is the replace of this line
+
+    document.getElementById('id_truebtn').onclick = function() {
+       // do your delete operation
+       //var checkout = window.confirm('Would you like to checkout or continue shopping?');
+       document.getElementById('id_confrmdiv').style.display = 'none';
+       document.getElementById('id_continuediv').style.display = 'block';
+       document.getElementById('truebtn2').onclick = function() {
+         document.getElementById('id_continuediv').style.display = 'none';
+       };
+       document.getElementById('falsebtn2').onclick = function() {
+         document.getElementById('id_continuediv').style.display = 'none';
+       };
+        //alert('true');
+    };
+
+    document.getElementById('id_falsebtn').onclick = function() {
+      console.log('Not checking you out');
+      document.getElementById('id_confrmdiv').style.display = 'none';
+         //alert('false');
+       return false;
+    };
   }
-
-
-
   postProductInfo() {
     this.data.getAllProductInfo().subscribe(
       data => this.items$ = data
@@ -63,7 +66,7 @@ export class ProductsPageComponent implements OnInit {
 
   ngOnInit() {
     this.postProductInfo();
-    this.data.currentStatus.subscribe(message => this.login_message = message)
+    this.data.currentStatus.subscribe(message => this.login_message = message);
   }
 
   signOut() {
